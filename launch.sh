02576e534c952e4a0bc0924a12b54b7e12d64e44
AlexMKramer <alexmkramer94@gmail.com>
2023-08-02 16:46:32 -0600

    updated launch.sh

--- launch.sh
+++ launch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+cd /home/alexk/Desktop/aiyabot/
+
 python -m venv venv
 
 source ./venv/bin/activate
 pip install -r requirements.txt
 
 python core/setup_generate.py
-python aiya.py
+sudo -u alexk python3 aiya.py
